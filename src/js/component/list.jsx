80a3c67cd5f9b0cd7b8c3dcee88e224aa2c0817f
xaviune <xaviune@gmail.com>
2023-01-23 18:24:55 +0000

    visibility en boton de borrar

--- src/js/component/list.jsx
+++ src/js/component/list.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import { XIcon } from "@primer/octicons-react";
 
 const List = (props) => {
 	const todos=props.todos;
 	const setTodos=props.setTodos;
+	const [Visibility, setVisivility] = useState({visibility: 'hidden'})
 	return (
 		<>
 		{todos.map ((item, index) => (
-			<div className="border border-secondary p-2"> {item.label}<div className="final"
+			<div className="border border-secondary p-2"
+			onMouseEnter={
+				e => {setVisivility({visibility: 'visible'})}
+			}
+			onMouseLeave={
+				e => {setVisivility({visibility: 'hidden'})}
+			}
+			> {item.label}<div className="final"
 			onClick={
 				() => 
 			setTodos(
@@ -15,7 +23,7 @@ const List = (props) => {
 					(t, currentIndex) =>
 						index != currentIndex
 			))}>
-				<XIcon size={24}/></div></div>
+				<div style={Visibility}><XIcon size={24}/></div></div></div>
 			))}
 		
 		</>
